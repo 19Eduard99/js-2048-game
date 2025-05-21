81ca6fe63259ddbd864c83df9826330b80376877
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2025-05-21 12:59:22 +0300

    create js-2048-game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+const Game = require("../modules/Game.class");
+const initialState = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+const game = new Game(initialState);
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+      game.moveLeft();
+      break;
+    case "ArrowRight":
+      game.moveRight();
+      break;
+    case "ArrowUp":
+      game.moveUp();
+      break;
+    case "ArrowDown":
+      game.moveDown();
+      break;
+  }
+});
